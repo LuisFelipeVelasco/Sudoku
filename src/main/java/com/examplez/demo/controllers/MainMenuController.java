04d9969c84316533b1538160fd58cb00fa753645
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-05-30 13:07:23 -0500

    Rename and Reorganize files

---
+++ src/main/java/com/examplez/demo/controllers/MainMenuController.java
@@ -0,0 +1,12 @@
+package com.examplez.demo.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainMenuController {
+
+    @FXML
+    protected void onButtonPlay() {
+
+    }
+}
