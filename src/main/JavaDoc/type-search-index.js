145386b5e35c098a0ac099eaffa6a0dcfc9cfaa9
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-06-27 18:11:41 -0500

    add JavaDoc

---
+++ src/main/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"com.examplez.demo","l":"GameLauncher"},{"p":"com.examplez.demo","l":"Launcher"},{"p":"com.examplez.demo.controllers","l":"MainMenuController"},{"p":"com.examplez.demo.models","l":"SudokuGame"},{"p":"com.examplez.demo","l":"SudokuInitializable","k":"10"}];updateSearchResults();
